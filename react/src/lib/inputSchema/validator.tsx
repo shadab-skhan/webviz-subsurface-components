6a5f01ada87191d23a27fc7ea1b43fb6eaad22cf
Shadab Khan <Shadab.Khan@Emerson.com>
2022-04-25 11:55:39 +0530

    add schema versioning

--- react/src/lib/inputSchema/validator.tsx
+++ react/src/lib/inputSchema/validator.tsx
@@ -1,5 +1,5 @@
 import Ajv from "ajv";
-import { ErrorObject, ValidateFunction } from "ajv/dist/types/index";
+import { ErrorObject, ValidateFunction, AnySchema } from "ajv/dist/types/index";
 
 // schema definations
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -12,18 +12,26 @@ const gridSchema = require("./Grid.json");
 const faultPolygonsSchema = require("./FaultPolygons.json");
 const colorTablesSchema = require("./ColorTables.json");
 
+type Schema = AnySchema & { version: string };
+
 // Validator function accepts parameter data and schema type to be validated against.
 // Throws error message of failure.
-export function validateSchema(data: unknown, schema_type: string): void {
+export function validateSchema(data: any, schema_type: string): void {
     let validator: ValidateFunction<unknown> | null = null;
 
     try {
         validator = createSchemaValidator(schema_type);
     } catch (e) {
-        throw "Wrong JSON schema for " + schema_type + ". " + String(e);
+        throw formatSchemaError(schema_type, String(e));
     }
 
-    if (!validator) throw "Wrong schema type.";
+    const required_schema_version = (validator.schema as Schema).version;
+    if (data["version"] !== required_schema_version) {
+        throw formatSchemaError(
+            schema_type,
+            `data should have 'version: ${required_schema_version}'`
+        );
+    }
 
     validator(data);
     if (validator.errors) {
@@ -31,12 +39,11 @@ export function validateSchema(data: unknown, schema_type: string): void {
     }
 }
 
-function createSchemaValidator(
-    schema_type: string
-): ValidateFunction<unknown> | null {
+function createSchemaValidator(schema_type: string): ValidateFunction<unknown> {
     const ajv = new Ajv({
         schemas: [wellLogSchema], // add list of dependent schemas
     });
+    ajv.addKeyword("version");
 
     switch (schema_type) {
         case "Wells":
@@ -56,18 +63,23 @@ function createSchemaValidator(
         case "ColorTables":
             return ajv.compile(colorTablesSchema);
         default:
-            return null;
+            throw "Wrong schema type";
     }
 }
 
-function formatSchemaError(schema_type: string, errors: ErrorObject[]): string {
+function formatSchemaError(
+    schema_type: string,
+    errors: ErrorObject[] | string
+): string {
     let error_text = "";
-
-    if (errors[0]) {
-        error_text =
-            (errors[0].dataPath ? errors[0].dataPath + ": " : "") +
-            errors[0].message;
-    } else error_text = "JSON schema validation failed";
-
-    return `${schema_type}: ${error_text}.`;
+    if (typeof errors === "object")
+        if (errors[0]) {
+            error_text =
+                (errors[0].dataPath ? errors[0].dataPath + ": " : "") +
+                errors[0].message;
+        } else error_text = "JSON schema validation failed";
+    else {
+        error_text = errors;
+    }
+    return `${schema_type}: ${error_text}`;
 }
